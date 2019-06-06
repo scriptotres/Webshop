1b7c8ecb7c4dd356441c6194f88b3927d34d443d
Emmanuel Benbrahim <emmanuel.benbrahim@outlook.com>
2019-06-06 15:12:20 +0200

    Product Repository Programmed

---
+++ Webshop.DataAccess.InMemory/Repositories/ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using Webshop.Core.Models;
+
+
+namespace Webshop.DataAccess.InMemory.Repositories
+{
+    public class ProductRepository
+    {
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        public Product FindProduct(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            var productToDelete = FindProduct(Id);
+            products.Remove(productToDelete);
+        }
+    }
+}
---
+++ Webshop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webshop.WebUI.MvcApplication" Language="C#" %>
